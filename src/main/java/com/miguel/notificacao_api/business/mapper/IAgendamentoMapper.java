47b9817af371680676dce0f69e63c5694f339855
gamerYToff <miguellopesdelmondes@gmail.com>
2025-02-14 22:33:38 -0300

    Adiciona métodos de cancelamento de notificações

--- src/main/java/com/miguel/notificacao_api/business/mapper/IAgendamentoMapper.java
+++ src/main/java/com/miguel/notificacao_api/business/mapper/IAgendamentoMapper.java
@@ -4,6 +4,8 @@ import com.miguel.notificacao_api.controller.dto.AgendamentoInDTO;
 import com.miguel.notificacao_api.controller.dto.AgendamentoOutDTO;
 import com.miguel.notificacao_api.infra.entity.Agendamento;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -12,4 +14,7 @@ public interface IAgendamentoMapper {
 
     Agendamento toEntity(AgendamentoInDTO inDTO);
     AgendamentoOutDTO toEntity(Agendamento agendamento);
+    @Mapping(target = "dataModificacao", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "statusNotificacao", expression = "java(StatusNotificacao.CANCELADO)")
+    Agendamento toEntityCancelado(Agendamento agendamento);
 }
--- src/main/java/com/miguel/notificacao_api/controller/dto/AgendamentoOutDTO.java
+++ src/main/java/com/miguel/notificacao_api/controller/dto/AgendamentoOutDTO.java
@@ -7,5 +7,5 @@ import java.time.LocalDateTime;
 
 public record AgendamentoOutDTO(Long id, String emailDestinatario, String telefoneDestinatario, String mensagem,
                                 @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-                                LocalDateTime dataEnvio, StatusNotificacao status) {
+                                LocalDateTime dataEnvio, StatusNotificacao statusNotificacao) {
 }
